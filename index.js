9c4cadd7e92750f1ff139fc5c056c829cf457191
juliocj360 <juliocj360@gmail.com>
2017-04-06 23:16:21 -0700

    User can see responsive website. #3

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const app = express();
-const request = require('request');
+const express = require('express')
+const app = express()
+const request = require('request')
 const parseString = require('xml2js').parseString
-const PORT = 3009;
+const PORT = 3009
 
 app.listen(PORT, () => {
   console.log('listening on port ' + PORT)
-});
+})
 
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 app.get('/feed/:id', (req, res) => {
-  request('https://queryfeed.net/twitter?q=' + req.params.id, function(err, response, body) {
+  request('https://queryfeed.net/twitter?q=' + req.params.id, (err, response, body) => {
+    if (err) {
+      console.log(err)
+      return
+    }
+    parseString(body, (err, result) => {
       if (err) {
         console.log(err)
         return
       }
-      parseString(body, function(err, result) {
-        if (err) {
-          console.log(err)
-          return
-        }
-        res.json(result)
-      })
+      res.json(result)
     })
   })
+})
